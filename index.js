3f53534cafd54a48644730baa8c56583a7aaffa1
kobelb <brandon.kobel@elastic.co>
2023-05-12 07:28:39 -0400

    Starting to use a .env file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const bodyParser = require('body-parser')
 const express = require('express');
 const fetch = require('node-fetch');
@@ -6,10 +8,7 @@ const uuid = require('uuid');
 const app = express();
 const port = 3000;
 
-const tenantId = "foo";
 const workerId = uuid.v4();
-
-const totalCapacity = 10;
 
 const runningTasks = new Set();
 
@@ -17,17 +16,35 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+const { TASK_MANAGER_URL } = process.env;
+if (TASK_MANAGER_URL == null) {
+    console.error(`TASK_MANAGER_URL environment variable should be set`);
+    return;
+}
+
+const { TENANT } = process.env;
+if (TENANT == null) {
+    console.error(`TENANT environment variable should be set`);
+    return;
+}
+
+const TOTAL_CAPACITY = parseInt(process.env.TOTAL_CAPACITY, 10);
+if (isNaN(TOTAL_CAPACITY)) {
+    console.error(`TOTAL_CAPACITY environment variable should be set to an number`);
+    return;
+}
+
 function startHeartbeat() {
     async function heartbeat () {
         const body = JSON.stringify({
             id: workerId,
-            tenant_id: tenantId,
+            tenant_id: TENANT,
             url: 'http://localhost:3000/run_task',
-            total_capacity: totalCapacity,
+            total_capacity: TOTAL_CAPACITY,
             running_tasks: Array.from(runningTasks),
         });
         try {
-            const response = await fetch(`http://localhost:1323/worker_heartbeat`, { method: 'POST', body, headers: { 'Content-Type': 'application/json'} });
+            const response = await fetch(`${TASK_MANAGER_URL}/worker_heartbeat`, { method: 'POST', body, headers: { 'Content-Type': 'application/json'} });
             if (response.status != 200) {
                 console.error(`Expected 200 status code, received ${response.status}: ${JSON.stringify(await response.json())}`)
             }
@@ -45,11 +62,11 @@ function startHeartbeat() {
 async function taskDone(taskId) {
     const body = JSON.stringify({
         id: taskId,
-        tenant_id: tenantId,
+        tenant_id: TENANT,
         worker_id: workerId,
     });
     try {
-        const response = await fetch(`http://localhost:1323/task/_done`, { method: 'POST', body, headers: { 'Content-Type': 'application/json'} });
+        const response = await fetch(`${TASK_MANAGER_URL}/task/_done`, { method: 'POST', body, headers: { 'Content-Type': 'application/json'} });
         if (response.status != 200) {
             console.error(`Expected 200 status code, received ${response.status}: ${JSON.stringify(await response.json())}`)
         }
@@ -62,12 +79,12 @@ async function taskDone(taskId) {
 async function taskFailure(taskId) {
     const body = JSON.stringify({
         id: taskId,
-        tenant_id: tenantId,
+        tenant_id: TENANT,
         worker_id: workerId,
         schedule_at: new Date(new Date().getTime() + 60000).toISOString()
     });
     try {
-        const response = await fetch(`http://localhost:1323/task/_failure`, { method: 'POST', body, headers: { 'Content-Type': 'application/json'} });
+        const response = await fetch(`${TASK_MANAGER_URL}/task/_failure`, { method: 'POST', body, headers: { 'Content-Type': 'application/json'} });
         if (response.status != 200) {
             console.error(`Expected 200 status code, received ${response.status}: ${JSON.stringify(await response.json())}`)
         }
@@ -82,15 +99,17 @@ function runTask(body) {
     console.log(new Date(), 'running task', { taskId });
     runningTasks.add(taskId);
     setTimeout(() => {
-        taskFailure(taskId);
-    }, 1000);
+        taskDone(taskId);
+    }, getRandomInt(2000));
 }
 
 app.use(bodyParser.json());
 
 app.post('/run_task', (req, res) => {
     runTask(req.body);
-    res.send();
+    setTimeout(() => {
+        res.send();
+    }, 30000);
 });
 
 app.listen(port, () => {
